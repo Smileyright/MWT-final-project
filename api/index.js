13bbf53c460e5d9eca76c3494c86545f40ad081f
Smileyright <alexplaysoccer880@gmail.com>
2025-12-08 00:29:28 -0500

    Revert "testing project changes"
    This reverts commit 7910be46d779d4f6730923ce80849977c90865cd.

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../server");
-const connectDB = require("../lib/mongo");
-
-module.exports = async (req, res) => {
-  await connectDB();     // connect per request (cached)
-  const handler = serverless(app);
-  return handler(req, res);
-};
